a5df428fb74467dbaeb6a33b4fc3f79943501cc5
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-02-29 17:11:01 +0000

    Initial commit

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        # Return the first 50 chars of the text field
+        return self.text[:50]
