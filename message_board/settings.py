fbc8cfd6515193ae2c265ec369412964517d9605
Jon Lee <39420293+4ka0@users.noreply.github.com>
2020-02-29 20:12:52 +0000

    Updated for Heroku deployment

--- message_board/settings.py
+++ message_board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'os2ziy)+v!$&j6ogf!3ie2@q2iicu(i5)b#q(#@jsrdnh2ulv$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
